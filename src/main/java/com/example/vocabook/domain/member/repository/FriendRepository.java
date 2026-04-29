23f7d5927ba28c1c338a84e886e1bf05d0699877
JuHeon <juheon0313@naver.com>
2026-04-29 15:41:20 +0900

    ✨ feat: Friend 엔티티 생성 & 필요한 객체 생성

---
+++ src/main/java/com/example/vocabook/domain/member/repository/FriendRepository.java
@@ -0,0 +1,55 @@
+package com.example.vocabook.domain.member.repository;
+
+import com.example.vocabook.domain.member.entity.Friend;
+import com.example.vocabook.domain.member.entity.Member;
+import com.example.vocabook.domain.member.enums.FriendState;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface FriendRepository extends JpaRepository<Friend, Long> {
+    @Query(
+            value = "SELECT * " +
+                    "FROM friend " +
+                    "WHERE (to_id = :toId AND friend_state != :friendState) AND friend_id < :cursor " +
+                    "ORDER BY friend_id DESC ",
+            nativeQuery = true
+    )
+    Slice<Friend> findFriendRequestListWithCursor(Long toId, FriendState friendState, Long cursor, PageRequest pageRequest);
+
+    @Query(
+            value = "SELECT * " +
+                    "FROM friend " +
+                    "WHERE to_id = :toId AND friend_state != :friendState " +
+                    "ORDER BY friend_id DESC ",
+            nativeQuery = true
+    )
+    Slice<Friend> findFriendRequestListWithoutCursor(Long toId, FriendState friendState, PageRequest pageRequest);
+
+    boolean existsByFromMemberAndToMember(Member member, Member friend);
+
+    boolean existsByFromMemberAndFriendStateIs(Member friend, FriendState friendState);
+
+    Optional<Friend> findByFromMemberAndToMember(Member fromMember, Member member);
+
+    @Query(
+            value = "SELECT * " +
+                    "FROM friend " +
+                    "WHERE (from_id = :fromId AND friend_state = :friendState) AND friend_id < :cursor " +
+                    "ORDER BY friend_id DESC ",
+            nativeQuery = true
+    )
+    Slice<Friend> findFriendListWithCursor(Long fromId, FriendState friendState, Long cursor, PageRequest pageRequest);
+
+    @Query(
+            value = "SELECT * " +
+                    "FROM friend " +
+                    "WHERE from_id = :fromId AND friend_state = :friendState " +
+                    "ORDER BY friend_id DESC ",
+            nativeQuery = true
+    )
+    Slice<Friend> findFriendListWithoutCursor(Long id, FriendState friendState, PageRequest pageRequest);
+}
