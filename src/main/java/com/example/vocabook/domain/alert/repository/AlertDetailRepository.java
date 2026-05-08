ad72d30ef0eb9daa407694bcacbd2f8f2a26e8f8
JuHeon <juheon0313@naver.com>
2026-05-08 12:54:46 +0900

    ✨ feat: 알림 목록 조회, 알림 삭제 API 구현

--- src/main/java/com/example/vocabook/domain/alert/repository/AlertDetailRepository.java
+++ src/main/java/com/example/vocabook/domain/alert/repository/AlertDetailRepository.java
@@ -3,10 +3,31 @@ package com.example.vocabook.domain.alert.repository;
 import com.example.vocabook.domain.alert.entity.Alert;
 import com.example.vocabook.domain.alert.entity.AlertDetail;
 import com.example.vocabook.domain.alert.enums.Repeat;
+import com.example.vocabook.domain.member.entity.Member;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface AlertDetailRepository extends JpaRepository<AlertDetail, Long> {
     boolean existsByContentAndAlertAndRepeatAndAlertedAt(String content, Alert alert, Repeat repeat, LocalDateTime alertedAt);
+
+    @Query(
+            value = "SELECT ad " +
+                    "FROM AlertDetail ad " +
+                    "LEFT JOIN ad.alert a " +
+                    "WHERE a.member = :member AND ad.id < :cursor "
+    )
+    Slice<AlertDetail> findAlertListWithCursor(Member member, Long cursor, Pageable pageRequest);
+
+    @Query(
+            value = "SELECT ad " +
+                    "FROM AlertDetail ad " +
+                    "LEFT JOIN ad.alert a " +
+                    "WHERE a.member = :member "
+    )
+    Slice<AlertDetail> findAlertListWithoutCursor(Member member, Pageable pageRequest);
 }
