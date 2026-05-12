414c08022aa76b1b8825eacf37ad5804b201f3c7
JuHeon <juheon0313@naver.com>
2026-05-13 01:31:33 +0900

    ✨ feat: 단어 CRUD 기능 추가

--- src/main/java/com/example/vocabook/domain/voca/converter/VocaConverter.java
+++ src/main/java/com/example/vocabook/domain/voca/converter/VocaConverter.java
@@ -1,12 +1,25 @@
 package com.example.vocabook.domain.voca.converter;
 
+import com.example.vocabook.domain.member.dto.req.AdminReqDTO;
 import com.example.vocabook.domain.voca.dto.VocaResDTO;
+import com.example.vocabook.domain.voca.entity.Voca;
 import com.example.vocabook.domain.voca.entity.Word;
 import org.springframework.data.domain.Page;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class VocaConverter {
+
+    public static Voca toVoca(
+            AdminReqDTO.AddVocabulary dto
+    ){
+        return Voca.builder()
+                .createdAt(LocalDateTime.now())
+                .description(dto.description())
+                .solvedCoin(dto.solvedCoin())
+                .build();
+    }
 
 	public static VocaResDTO.WordList toWordList(Page<Word> wordPage) {
 		List<VocaResDTO.WordInfo> words = wordPage.getContent().stream()
